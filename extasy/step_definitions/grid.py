f97aeada00dfb1d23726c52b26eea889a11a08ab
Rafael Martins <rafael.martins@ispm.com.br>
2010-11-11 15:00:16 -0200

    Implemented actions to check grid line/column values

--- extasy/step_definitions/grid.py
+++ extasy/step_definitions/grid.py
@@ -51,65 +51,10 @@ def _get_line_accessor( line ):
     if extasy.lang.get( 'last' ) == line:
         return 'last()'
         
-    return re.sub( '\D+', '', line ) 
-    
-
-@Given( \
-    'I wait for grid to be present', \
-    'I wait for "$title" grid to be present', \
-    'I wait for grid to be present for $timout seconds', \
-    'I wait for "$title" grid to be present for $timout seconds'
-)
-@When( \
-    'I wait for grid to be present', \
-    'I wait for "$title" grid to be present', \
-    'I wait for grid to be present for $timout seconds', \
-    'I wait for "$title" grid to be present for $timout seconds' \
-)
-@Then( \
-    'I wait for grid to be present', \
-    'I wait for "$title" grid to be present', \
-    'I wait for grid to be present for $timout seconds', \
-    'I wait for "$title" grid to be present for $timout seconds' \
-)
-def wait_for_presence( context, title = None, timeout = None ):
-    xpath = 'xpath=%s' % _getxpath( title = title )
-
-    if not timeout:
-        timeout = extasy.DEFAULT_WAIT_FOR_PRESENCE_TIMEOUT
-    timeout = int( timeout )
-
-    if not extasy.selenium.getDriver().wait_for_element_present(xpath, timeout):
-        if title:
-            message = '"%s" grid should appear in %s seconds' % ( title, timeout )
-        else:
-            message = 'grid should appear in %s seconds' % ( timeout )
-        raise StepFailure( message )
-
-
-@Given( \
-    'I wait for grid lines to be present', \
-    'I wait for grid lines to be present for $timout seconds', \
-    'I wait for "$title" grid lines to be present', \
-    'I wait for "$title" grid lines to be present for $timout seconds' \
-)
-@When( \
-    'I wait for grid lines to be present', \
-    'I wait for grid lines to be present for $timout seconds', \
-    'I wait for "$title" grid lines to be present', \
-    'I wait for "$title" grid lines to be present for $timout seconds' \
-)
-@Then( \
-    'I wait for grid lines to be present', \
-    'I wait for grid lines to be present for $timout seconds', \
-    'I wait for "$title" grid lines to be present', \
-    'I wait for "$title" grid lines to be present for $timout seconds', \
-    'grid lines should appear', \
-    'grid lines should appear in $timeout seconds', \
-    '"$title" grid lines should appear', \
-    '"$title" grid lines should appear in $timeout seconds' \
-)
-def lines_wait_for_presence( context, title = None, timeout = None ):
+    return re.sub( '\D+', '', line )
+  
+  
+def _lines_wait_for_presence( context, title = None, timeout = None ):
     xpath = 'xpath=%s' % _get_lines_xpath( title = title )
 
     if not timeout:
@@ -123,6 +68,22 @@ def lines_wait_for_presence( context, title = None, timeout = None ):
             message = 'lines of grid should appear in %s seconds' % ( timeout )
         raise StepFailure( message )  
  
+    
+def _wait_for_presence( context, title = None, timeout = None ):
+    xpath = 'xpath=%s' % _getxpath( title = title )
+
+    if not timeout:
+        timeout = extasy.DEFAULT_WAIT_FOR_PRESENCE_TIMEOUT
+    timeout = int( timeout )
+
+    if not extasy.selenium.getDriver().wait_for_element_present(xpath, timeout):
+        if title:
+            message = '"%s" grid should appear in %s seconds' % ( title, timeout )
+        else:
+            message = 'grid should appear in %s seconds' % ( timeout )
+        raise StepFailure( message )
+
+    
 def _line_click( context, title = None, line = None, dblclick = False ):
     line = _get_line_accessor( line )
     xpath = 'xpath=%s' % _get_line_xpath( title = title, line = line )
@@ -138,16 +99,82 @@ def _line_click( context, title = None, line = None, dblclick = False ):
         extasy.selenium.getDriver().double_click_element_at(xpath, 1, 1)
     else:
         extasy.selenium.getDriver().click_element_at(xpath, 1, 1)
- 
     
-@Given( 'I click on $line line of grid', 'I click on $line line of "$title" grid' )
-@When( 'I click on $line line of grid', 'I click on $line line of "$title" grid' )
-@Then( 'I click on $line line of grid', 'I click on $line line of "$title" grid' )
-def line_click( context, title = None, line = None ):
-    return _line_click( context, title = None, line = None, dblclick = False )
+
+@Given( 'I wait for "$title" grid to be present' )
+@When( 'I wait for "$title" grid to be present' )
+@Then( 'I wait for "$title" grid to be present' )
+def wait_for_presence( context, title ):
+    return _wait_for_presence( context, title )
+        
+@Given( 'I wait for grid to be present' )
+@When( 'I wait for grid to be present' )
+@Then( 'I wait for grid to be present' )
+def wait_for_presence_no_title( context ):
+    return _wait_for_presence( context )
+
+
+@Given( 'I wait for "$title" grid to be present for $timout seconds' )
+@When( 'I wait for "$title" grid to be present for $timout seconds' )
+@Then( 'I wait for "$title" grid to be present for $timout seconds' )
+def wait_for_presence_timeout( context, title, timeout = None ):
+    return _wait_for_presence( context, title, timeout )
+
+@Given( 'I wait for grid to be present for $timout seconds' )
+@When( 'I wait for grid to be present for $timout seconds' )
+@Then( 'I wait for grid to be present for $timout seconds' )
+def wait_for_presence_timeout_no_title( context, timeout = None ):
+    return _wait_for_presence( context, None, timeout )
     
-@Given( 'I doubleclick $line line of grid', 'I doubleclick $line line of "$title" grid' )
-@When( 'I doubleclick $line line of grid', 'I doubleclick $line line of "$title" grid' )
-@Then( 'I doubleclick $line line of grid', 'I doubleclick $line line of "$title" grid' )
-def line_doubleclick( context, title = None, line = None ):
+
+@Given( 'I wait for "$title" grid lines to be present' )
+@When( 'I wait for "$title" grid lines to be present' )
+@Then( 'I wait for "$title" grid lines to be present' )
+def lines_wait_for_presence( context, title ):
+    return _lines_wait_for_presence( context, title )
+
+@Given( 'I wait for grid lines to be present' )
+@When( 'I wait for grid lines to be present' )
+@Then( 'I wait for grid lines to be present' )
+def lines_wait_for_presence_no_title( context ):
+    return _lines_wait_for_presence( context )
+
+
+@Given( 'I wait for "$title" grid lines to be present for $timout seconds' )
+@When( 'I wait for "$title" grid lines to be present for $timout seconds' )
+@Then( 'I wait for "$title" grid lines to be present for $timout seconds' )
+def lines_wait_for_presence_timeout( context, title, timeout = None ):
+    return _lines_wait_for_presence( context, title, timeout )
+
+@Given( 'I wait for grid lines to be present for $timout seconds' )
+@When( 'I wait for grid lines to be present for $timout seconds' )
+@Then( 'I wait for grid lines to be present for $timout seconds' )
+def lines_wait_for_presence_timeout_no_title( context, timeout = None ):
+    return _lines_wait_for_presence( context, None, timeout )
+    
+    
+@Given( 'I click on $line line of grid' )
+@When( 'I click on $line line of grid' )
+@Then( 'I click on $line line of grid' )
+def line_click_no_title( context, line ):
+    return _line_click( context, line=line )
+    
+@Given( 'I click on $line line of "$title" grid' )
+@When( 'I click on $line line of "$title" grid' )
+@Then( 'I click on $line line of "$title" grid' )
+def line_click( context, line, title ):
+    return _line_click( context, title=title, line=line )
+    
+    
+@Given( 'I doubleclick $line line of grid' )
+@When( 'I doubleclick $line line of grid' )
+@Then( 'I doubleclick $line line of grid' )
+def line_doubleclick_no_title( context, line ):
     return _line_click( context, line=line, dblclick = True )
+
+
+@Given( 'I doubleclick $line line of "$title" grid' )
+@When( 'I doubleclick $line line of "$title" grid' )
+@Then( 'I doubleclick $line line of "$title" grid' )
+def line_doubleclick( context, line, title ):
+    return _line_click( context, title=title, line=line, dblclick = True )
